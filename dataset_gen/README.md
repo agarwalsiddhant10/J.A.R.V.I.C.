7083be80e9979f97c30e3d0cf1be20feed735e06
Siddhant Agarwal <agarwalsiddhant10@gmail.com>
2019-03-31 22:29:12 +0530

    Add the dataset generation codes

---
+++ dataset_gen/README.md
@@ -0,0 +1,26 @@
+# Data Generation
+To generate data first set the host and port of both server and client
+
+    - The Client code is Client.java
+    - After editing Client.java, compile it.
+    - Run the Client code
+
+For the server code i.e. dataset_gen.py
+
+    - First of all adjust the file name
+    - Set the host and port
+    - Run the code
+
+## Note:
+
+Run the server code before running the Client
+Close the Client code before closing Server
+
+There are some exceptions and errors that have not been handled well now but the code can be used to generate data without much trouble.
+
+## REMEMBER:
+
+In case you close the server before the client the port will still remain active
+Then change the ports in both the code and use normally
+
+
---
+++ dataset_gen/dataset_gen.py
@@ -0,0 +1,52 @@
+from server_class import Server 
+from _thread import *
+import threading
+import sys
+
+print_lock = threading.Lock()
+
+def send_msg(server, file_name):
+    while True:
+        file_server = open(file_name + "_server.txt", "a")
+        msg = input("\n")
+        server.send_msg(msg)
+        file_server.write(msg + "\n")
+        file_server.close()
+        
+def recieve_msg(server, file_name):
+    while True:
+        file_client = open(file_name + "_client.txt", "a")
+        msg = server.recieve_msg()
+        if msg == "Nan":
+            print_lock.release()
+            file_client.close()
+            sys.exit()
+        print("Client: " + msg)
+        file_client.write(msg)
+        file_client.close()
+    
+
+def chat(server, file_name):
+    while True:
+        print_lock.acquire()
+
+        start_new_thread(send_msg,(server,file_name))
+        start_new_thread(recieve_msg,(server, file_name))
+
+def main():
+    host = 'localhost'
+    port = 2003
+
+    file_name = "Siddhant_Rajat_Happy" #<Server>_<Client>_<Emotion of the client>
+    server = Server(host, port)
+    server.initialise_connection()
+    chat(server, file_name)
+
+
+if __name__ =='__main__':
+    main()
+        
+
+
+
+
